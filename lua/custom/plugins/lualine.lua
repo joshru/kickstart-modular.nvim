30a5900dda4dada27987171b8f2541c9734482be
Josh Rueschenberg <joshua.rueschenberg@navy.mil>
2024-05-17 05:04:15 -0700

    Switch from mini statusbar to lualine

---
+++ lua/custom/plugins/lualine.lua
@@ -0,0 +1,12 @@
+return {
+  'nvim-lualine/lualine.nvim',
+  opts = {
+    options = {
+      -- theme = 'dracula',
+      theme = 'catppuccin-macchiato',
+      refresh = {
+        statusline = 1000,
+      },
+    },
+  },
+}
